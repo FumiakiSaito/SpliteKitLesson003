56a4cfadf8d5ffcd44991d7acf845bff8dc2c980
plssaito <s-fumiaki@plusplus.jp>
2015-03-25 01:18:13 +0900

    first commit

---
+++ SpliteKitLesson003/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  SpliteKitLesson003
+//
+//  Created by Fumiaki Saito on 2015/03/25.
+//  Copyright (c) 2015年 mycompany. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
